130196df0d30e8a0c5838921e6e7c53ac39bf566
PudimAtomico <kauanaraujoramos@gmail.com>
2024-03-06 22:30:19 -0300

    Adicionado header e menu mobile

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 const botao = document.querySelectorAll(".botao");
 const resposta = document.querySelectorAll(".resposta");
+let meuInput = document.getElementById("meuinput");
+meuInput.addEventListener("", () => {
+    console.log("meuInput.value")
+});
 
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
 
 /*ao apertar o botão, é adicionada a classe "ativo" a ele, mudando a cor.*/
-
-function input(){ 
-    if (input != null && input % 2 == 0) {
-        resposta[0].classList.add("ativo");
-        resposta[1].classList.remove("ativo");
-    }   
-    else if (input != null) {
-        resposta[0].classList.remove("ativo");
-        resposta[1].classList.add("ativo");
-    }
-}
 
 for(let i=0;i <botao.length;i++){
 
@@ -23,19 +33,28 @@ for(let i=0;i <botao.length;i++){
 
         for(let j=0;j<botao.length;j++){
             botao[j].classList.remove("ativo");
-        }
+          
 
-        botao[i].classList.add("ativo");
+            botao[i].classList.add("ativo");
 
-       botao[i].onmousedown = function (){
+            botao[i].onmousedown = function (){
+                meuInput.addEventListener("keydown", function(){
+                if (meuInput.value % 2 == 0) {
+                    resposta[0].classList.add("ativo");
+                    resposta[1].classList.remove("ativo");
+                }   
+                else (meuInput.value != null); {
+                    resposta[0].classList.remove("ativo");
+                    resposta[1].classList.add("ativo");
+            }
+        })};
            
-        }
-    }
-    /*ao soltar o botão do mouse, a classe "ativo" é removida, deixando o botão com a cor padrão mais uma vez.*/
-    botao[i].onmouseup = function(){
+/*ao soltar o botão do mouse, a classe "ativo" é removida, deixando o botão com a cor padrão mais uma vez.*/
+        botao[i].onmouseup = function(){
         
-        for(let j=0;j<botao.length;j++){
-            botao[j].classList.remove("ativo");
-        }
-    }
-}
+                botao[j].classList.remove("ativo");
+            }
+        } 
+    } 
+} 
+console.log(meuInput)
